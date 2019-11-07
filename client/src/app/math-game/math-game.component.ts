5a3272dc0024cb2049ee5fae9cb79a86993c8952
Yolo2019-VAC <vyachzakhariiv@gmail.com>
2019-11-07 18:36:07 +0200

    Lesson 08

--- client/src/app/math-game/math-game.component.ts
+++ client/src/app/math-game/math-game.component.ts
@@ -7,13 +7,13 @@ import { Component, OnInit, HostListener } from '@angular/core';
 })
 export class MathGameComponent implements OnInit {
 
-
+  SxS = 4;
   msg='none';
   board = [
    [ 0, 0, 0, 0 ],
-   [ 0, 2, 0, 0 ],
-   [ 0, 0, 0, 0 ],
-   [ 0, 0, 0, 2 ]
+   [ 0, 2, 0, 2 ],
+   [ 0, 0, 2, 0 ],
+   [ 2, 0, 0, 2 ]
   ];
 
   constructor() { }
@@ -23,7 +23,40 @@ export class MathGameComponent implements OnInit {
 
   @HostListener('window:keydown.arrowup')
   onUp() {
-   console.log ( 'up' );
+    // sum numbers
+    // shift numbers
+    for (let c=0; c<this.SxS; c++) {
+      for (let r=this.SxS-1; r>0; r--) {
+        //step1
+        if ( this.board[r][c]==0) {
+          let find_rNo_ne_0 = -1;
+          for ( let ri=r+1; ri; ri++){
+            if ( this.board[ri][c]>0) {
+              find_rNo_ne_0 = ri;
+              break;
+            }
+          }
+         if (find_rNo_ne_0>0) {
+           this.board[r][c]=this.board[find_rNo_ne_0][c];
+           this.board[find_rNo_ne_0][c] = 0;
+         }
+        }
+        //step2
+        let find_rNo_eq_0 = -1;
+        for ( let rj=r+1; rj<this.SxS; rj++) {
+          if ( this.board[rj][c]==this.board[r][c]) {
+            find_rNo_eq_0 = rj;
+          }
+          if ( this.board[rj][c]>0) {
+            break;
+          }
+        }
+        if ( find_rNo_eq_0>0) { 
+          this.board[r][c]+=this.board[find_rNo_eq_0][c];
+          this.board[find_rNo_eq_0][c] = 0;
+        }
+      }
+    }
   }
   @HostListener('window:keydown.arrowright')
   onRight() {

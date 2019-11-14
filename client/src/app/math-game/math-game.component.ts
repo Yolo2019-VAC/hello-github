01070c0378a9d0c6a62fbe413f26650ecf974574
Yolo2019-VAC <vyachzakhariiv@gmail.com>
2019-11-14 18:43:25 +0200

    Lesson 09

--- client/src/app/math-game/math-game.component.ts
+++ client/src/app/math-game/math-game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { VirtualTimeScheduler } from 'rxjs';
 
 @Component({
   selector: 'app-math-game',
@@ -10,65 +11,95 @@ export class MathGameComponent implements OnInit {
   SxS = 4;
   msg='none';
   board = [
-   [ 0, 0, 0, 0 ],
+   [ 2, 2, 2, 0 ],
    [ 0, 2, 0, 2 ],
-   [ 0, 0, 2, 0 ],
+   [ 2, 0, 0, 0 ],
    [ 2, 0, 0, 2 ]
   ];
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
-  @HostListener('window:keydown.arrowup')
-  onUp() {
-    // sum numbers
-    // shift numbers
+  mover( a ) {
     for (let c=0; c<this.SxS; c++) {
       for (let r=this.SxS-1; r>0; r--) {
         //step1
-        if ( this.board[r][c]==0) {
+        if ( a[ r ] ==0 ) {
           let find_rNo_ne_0 = -1;
-          for ( let ri=r+1; ri; ri++){
-            if ( this.board[ri][c]>0) {
+          for ( let ri=r+1; ri<this.SxS; ri++ ){
+            if ( a [ ri ] > 0) {
               find_rNo_ne_0 = ri;
               break;
             }
           }
          if (find_rNo_ne_0>0) {
-           this.board[r][c]=this.board[find_rNo_ne_0][c];
-           this.board[find_rNo_ne_0][c] = 0;
+           a [ r ] = a[find_rNo_ne_0];
+           a [ find_rNo_ne_0 ] = 0;
          }
         }
         //step2
         let find_rNo_eq_0 = -1;
         for ( let rj=r+1; rj<this.SxS; rj++) {
-          if ( this.board[rj][c]==this.board[r][c]) {
+          if ( a[rj] == a[r]) {
             find_rNo_eq_0 = rj;
           }
-          if ( this.board[rj][c]>0) {
+          if ( a [rj] > 0 ) {
             break;
           }
         }
-        if ( find_rNo_eq_0>0) { 
-          this.board[r][c]+=this.board[find_rNo_eq_0][c];
-          this.board[find_rNo_eq_0][c] = 0;
+        if ( find_rNo_eq_0 > 0) { 
+          a[r] += a[ find_rNo_eq_0 ];
+          a[ find_rNo_eq_0 ] = 0;
         }
       }
+      return a;
     }
+  }
+  getCol( cNo ) {
+    let a =[];
+    for ( let r=0; r <this.SxS; r++) {
+      a.push( this.board[ r ][ cNo ] );
+    }
+    return a;
+  }
+
+  setCol( cNo, a ) {
+    for ( let r=0; r <this.SxS; r++) {
+      this.board[ r ][ cNo ] = a[ r ];
+    }
+  }
+  getRow( rNo ) {
+    return this.board[ rNo ];
+  }
+  setRow( rNo, a ) {
+    this.board[ rNo ] = a;
+  }
+
+  ngOnInit() {
+   // this.setRow( 3, [ 16,32,16,16] );
+   // this.setRow( 3, this.getRow( 1 ) );
+  }
+
+  @HostListener('window:keydown.arrowup')
+  onUp() {
+   for ( let c=0; c < this.SxS; c++ ) {
+     this.setCol( c, this.mover( this.getCol( c ) ) );
+   }
   }
   @HostListener('window:keydown.arrowright')
   onRight() {
-   console.log ( 'right' );
+   for ( let r=0; r < this.SxS; r++ ) {
+     this.setRow ( r, this.mover(this.getRow( r ).reverse() ).reverse() );
+   }
   }
   @HostListener('window:keydown.arrowdown')
   onDown() {
-   console.log ( 'down' );
+    
   }
   @HostListener('window:keydown.arrowleft')
   onLeft() {
-   console.log ( 'left' );
+   for (let r=0; r < this.SxS; r++) {
+     this.setRow( r, this.mover( this.getRow ( r ) ) );
+   }
   }
-  
-}
+
+} 
